c7b6c59aafa449f4d7dc96a95ac7d6d1cc9a6c44
Tron404 <edi.raul@yahoo.com>
2025-03-06 15:58:19 +0100

    added some documentation for data loading/processing:

--- data_processing.py
+++ data_processing.py
@@ -6,26 +6,35 @@ from torch_geometric.data import Data
 from torch_geometric.datasets import DGraphFin
 from torch_geometric.loader import NeighborLoader
 from torch_geometric.transforms import RandomNodeSplit
-
-from typing import Literal
+from typing import Literal, Tuple, Iterable
 
 ### CREATE GRAPH
 
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-def make_node(data: pd.DataFrame, **kwargs):
+def make_node(data: pd.DataFrame, **kwargs) -> Tuple[torch.Tensor, dict]:
+    """
+    Create a graph-node tensor based on some given features, and create an address->index mapping
+    """
     mapping_idx = {node_address: idx for idx, node_address in enumerate(data.address.unique())}
                    
     x = torch.as_tensor(data.drop("address", axis=1).to_numpy(), dtype=torch.float32)
 
     return x, mapping_idx
 
-def make_edge(data, address_mapping):
+def make_edge(data: pd.DataFrame, address_mapping: dict) -> torch.Tensor:
+    """
+    Create an edge between two nodes (source/input -> target/output)
+    """
     edge_index = torch.as_tensor([[address_mapping[input_node], address_mapping[output_node]] for input_node, output_node in zip(data["input_address"], data["output_address"]) if input_node in address_mapping.keys() and output_node in address_mapping.keys()])
 
     return edge_index.T
 
-def make_masks(graph: Data, labels_to_remove: list[int]=None):
+def make_masks(graph: Data, labels_to_remove: list[int]=None) -> Data:
+    """
+    Create a training, validation, and testing mask for binary node classification;
+    Done because some node labels represent unknown/structural nodes that hold no label of interest
+    """
     node_split = RandomNodeSplit(num_val=0.1, num_test=0.5)
     graph = node_split(graph)
 
@@ -38,9 +47,15 @@ def make_masks(graph: Data, labels_to_remove: list[int]=None):
     
     return graph
 
-def make_graph(feature_df, edge_df, y_labels, save_to_disk=False, file_name=None):
+def make_graph(feature_df: pd.DataFrame, edge_df: pd.DataFrame, y_labels: Iterable, save_to_disk: bool=False, file_name: str=None) -> Data:
+    """
+    Create a graph with node and edge data, and ground truth labels
+    """
     x_features, node_mapping = make_node(feature_df)
     edge_index = make_edge(edge_df, node_mapping)
+
+    if not isinstance(y_labels, torch.Tensor):
+        y_labels = torch.as_tensor(y_labels)
 
     graph = Data()
     graph.x = x_features
@@ -58,16 +73,22 @@ def make_graph(feature_df, edge_df, y_labels, save_to_disk=False, file_name=None
 
 ### LOAD DATASETS
 
-def load_dgraphfin(path_to_folder="."):
+def load_dgraphfin(path_to_folder: str=".") -> Tuple[Data, int]:
+    """
+    Load the DGraphFin dataset from disk
+    """
     NUM_CLASSES = 2
     dataset = DGraphFin(root=os.path.join(path_to_folder, "dataset"))[0].to(DEVICE)
     dataset.pop("edge_type")
     dataset.pop("edge_time") # @TODO: check how the edges change over time (if at all?)
-    dataset.x = normalize_column(dataset.x)
+    dataset.x = normalize_column(dataset.x, norm_type="z-score")
 
     return dataset, NUM_CLASSES
 
-def load_create_ellipticpp(path_to_folder=".", save_to_disk=False, file_name=None):
+def load_create_ellipticpp(path_to_folder: str=".", save_to_disk: bool=False, file_name: str=None, timestep: int=1) -> Tuple[Data, int]:
+    """
+    Load the Elliptic++ dataset from disk and create a `torch_geometric.data.Data`
+    """
     NUM_CLASSES = 2
     addr2addr = os.path.join(path_to_folder, "Elliptic++Dataset/AddrAddr_edgelist.csv")
     wallets_features_classes = os.path.join(path_to_folder, "Elliptic++Dataset/wallets_features_classes_combined.csv")
@@ -75,7 +96,7 @@ def load_create_ellipticpp(path_to_folder=".", save_to_disk=False, file_name=Non
     addr2addr_df = pd.read_csv(addr2addr)
     wallets_features_classes_df = pd.read_csv(wallets_features_classes)
     wallets_features_classes_df = wallets_features_classes_df[
-        wallets_features_classes_df["Time step"] == 1
+        wallets_features_classes_df["Time step"] == timestep
     ]
     y_label_wallet = torch.as_tensor(wallets_features_classes_df["class"].tolist())
     y_label_wallet -= 1 # pytorch expects class labels in the range of [0, num_classes-1]
@@ -89,12 +110,18 @@ def load_create_ellipticpp(path_to_folder=".", save_to_disk=False, file_name=Non
 
 ### PROCESS DATASETS
 
-def normalize_column(data_feature, norm_type: Literal["z-score", "min-max"]="z-score"):
+def normalize_column(data_feature: Iterable, norm_type: Literal["z-score", "min-max"]="z-score") -> torch.Tensor:
+    """
+    Normalize a 1D Iterable (e.g., list, or 1D tensor) based on some scorign method
+    """
     def z_scoring(x: torch.Tensor):
         return (x - x.mean(dim=0))/x.std(dim=0)
     
     def minmax_scoring(x: torch.Tensor):
         return (x - x.min(dim=0))/(x.max(dim=0) - x.min(dim=0))
+
+    if not isinstance(data_feature, torch.Tensor):
+        data_feature = torch.as_tensor(data_feature)
 
     match norm_type:
         case "z-score":
@@ -107,7 +134,10 @@ def normalize_column(data_feature, norm_type: Literal["z-score", "min-max"]="z-s
 
     return data_feature
 
-def split_into_batches(graph: Data, num_batches, num_neighbours, num_hops, shuffle=False):
+def split_into_batches(graph: Data, num_batches: int, num_neighbours: int, num_hops: int, shuffle: bool=False) -> NeighborLoader:
+    """
+    Split a graph of type torch_geometric.data.Data into batches
+    """
     loader = NeighborLoader(
         data=graph,
         num_neighbors=[num_neighbours] * num_hops,
