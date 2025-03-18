db37719e1d1f518b0d6b63c7455bfa54313cf581
Tron404 <edi.raul@yahoo.com>
2025-03-18 13:29:22 +0100

    new attempt at env

--- setup_env.sh
+++ setup_env.sh
@@ -8,17 +8,20 @@ while getopts "d:c:" flag; do
     esac
 done
 
+# default packages, with no special versioning
+pip install seaborn scikit-learn pandas
+
 if [[ $device_flag == "cpu" ]]; then
-    pip install -r requirements_cpu.txt
+    pip install torch==2.4.0 --index-url https://download.pytorch.org/whl/cpu
+    pip install torch_geometric
+    pip install pyg_lib torch_scatter torch_sparse -f https://data.pyg.org/whl/torch-2.4.0+cpu.html
+
 elif [[ $device_flag == "gpu" ]]; then
     if [[ $cuda_flag == "" ]]; then
         error_line="Error: provide a cuda version when using the GPU."
         >&2 echo -e "\e[01;31m$error_line\e[0m"
         exit 1
     fi
-
-    # default packages, with no special versioning
-    pip install seaborn scikit-learn pandas
 
     # torch-related packages
     pip install torch==2.4.0 --index-url https://download.pytorch.org/whl/${cuda_flag}
