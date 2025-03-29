109cd597fead3380b02557f7ac5373b48981fb7d
kostaszavas <kostaszavas@gmail.com>
2025-03-29 21:03:58 +0100

    Update sage.py

--- model_scripts/sage.py
+++ model_scripts/sage.py
@@ -4,10 +4,25 @@ from torch.nn import Linear, ParameterList, Module
 from torch_geometric.nn import SAGEConv
 
 class SAGE(Module):
-    def __init__(self):
+    def __init__(self, in_dim, out_dim,**layer_paras):
         super().__init__()
+        self.hidden_dim = layer_paras.pop("hidden_size")
 
-        pass
+        self.conv_layers = []
+        self.conv_layers +=[SAGEConv(in_dim, self.hidden_dim)]
+        self.conv_layers +=[SAGEConv(self.hidden_dim, self.hidden_dim)]
 
+        self.conv_layers = ParameterList(self.conv_layers)
+
+        self.mlp1 = Linear(self.hidden_dim, self.hidden_dim//2)
+        self.mlp2 = Linear(self.hidden_dim//2, out_dim)
     def forward(self, x: torch.Tensor, edge_index: torch.Tensor) -> torch.Tensor:
-        pass
+        for conv in self.conv_layers:
+            x = conv(x, edge_index)
+            x = F.relu(x)
+
+        x = self.mlp1(x)
+        x = F.relu(x)
+        x = self.mlp2(x)
+
+        return x
